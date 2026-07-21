23b97757b98e126c3cc4a9cb5fd099885401b624
wdemozzi <wdteste88@gmail.com>
2026-07-21 08:32:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    // Aumenta o limite para 1500 kB caso alguma dependência isolada ainda seja grande
-    chunkSizeWarningLimit: 1500,
-    rolldownOptions: {
-      output: {
-        codeSplitting: {
-          minSize: 10000, 
-          groups: [
-            {
-              name: 'vendor',
-              // Regex corrigida sem espaços para garantir a captura na Vercel (Linux)
-              test: /[\\/]node_modules[\\/]/,
-              priority: 10,
-            },
-          ],
-        },
-      },
     },
   },
 })
